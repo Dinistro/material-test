567d4cbe85f9720da4bd50510f7260336d9c1df1
Christian Ulmann <christian.ulmann@bit.admin.ch>
2016-09-28 01:47:28 +0200

    Added mockgoose

--- server/mongo/db.ts
+++ server/mongo/db.ts
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose');
+var mockgoose = require('mockgoose');
 
+mockgoose(mongoose);
 export const connection = mongoose.connect('mongodb://localhost:27017/material-test');
 
