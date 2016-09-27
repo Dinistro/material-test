53988fb85fdb8a4922ad7f3e57e6d64808f6af6a
Pedro De Almeida <pedro@almeida.ch>
2016-09-27 22:53:38 +0200

    Add sample view for Material.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MdSidenav, MdDialog, MdDialogConfig} from "@angular/material";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Material app works!';
+
+  dogs = [
+    {rows: 2, name: "Mal", human: "Jeremy", age: 5},
+    {rows: 1, name: "Molly", human: "David", age: 5},
+    { rows: 1, name: "Sophie", human: "Alex", age: 8},
+    {rows: 2, name: "Taz", human: "Joey", age: '11 weeks'},
+    { rows: 1, name: "Kobe", human: "Igor", age: 5},
+    {rows: 2, name: "Porter", human: "Kara", age: 3},
+    { rows: 1, name: "Stephen", human: "Stephen", age: 8},
+    {rows: 1, name: "Cinny", human: "Jules", age: 3},
+    { rows: 1, name: "Hermes", human: "Kara", age: 3},
+  ];
 }
