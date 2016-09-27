530b7d49bbbd2c4d8c84d79eee16cadd2f98c97a
Pedro De Almeida <pedro@almeida.ch>
2016-09-27 23:13:47 +0200

    Move UI to Home view.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {MdSidenav, MdDialog, MdDialogConfig} from "@angular/material";
 
 @Component({
   selector: 'app-root',
@@ -7,17 +6,4 @@ import {MdSidenav, MdDialog, MdDialogConfig} from "@angular/material";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Material app works!';
-
-  dogs = [
-    {rows: 2, name: "Mal", human: "Jeremy", age: 5},
-    {rows: 1, name: "Molly", human: "David", age: 5},
-    { rows: 1, name: "Sophie", human: "Alex", age: 8},
-    {rows: 2, name: "Taz", human: "Joey", age: '11 weeks'},
-    { rows: 1, name: "Kobe", human: "Igor", age: 5},
-    {rows: 2, name: "Porter", human: "Kara", age: 3},
-    { rows: 1, name: "Stephen", human: "Stephen", age: 8},
-    {rows: 1, name: "Cinny", human: "Jules", age: 3},
-    { rows: 1, name: "Hermes", human: "Kara", age: 3},
-  ];
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef, OnInit } from '@angular/core';
+import {MdSidenav, MdDialog, MdDialogConfig} from "@angular/material";
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  constructor(public dialog: MdDialog, public vcr: ViewContainerRef) {}
 
-  constructor() { }
+  title = 'Material app works!';
 
-  ngOnInit() {
+  @ViewChild('sidenav') sidenav: MdSidenav;
+
+  dogs = [
+    {rows: 2, name: "Mal", human: "Jeremy", age: 5},
+    {rows: 1, name: "Molly", human: "David", age: 5},
+    { rows: 1, name: "Sophie", human: "Alex", age: 8},
+    {rows: 2, name: "Taz", human: "Joey", age: '11 weeks'},
+    { rows: 1, name: "Kobe", human: "Igor", age: 5},
+    {rows: 2, name: "Porter", human: "Kara", age: 3},
+    { rows: 1, name: "Stephen", human: "Stephen", age: 8},
+    {rows: 1, name: "Cinny", human: "Jules", age: 3},
+    { rows: 1, name: "Hermes", human: "Kara", age: 3},
+  ];
+
+  currentDog = {};
+
+  openDialog() {
+    const config = new MdDialogConfig();
+    config.viewContainerRef = this.vcr;
+    this.dialog.open(SettingsDialog, config);
   }
+
+  showDog(dog) {
+    this.currentDog = dog;
+    this.sidenav.open();
+  }
+
+  ngOnInit() {}
+}
+
+export class SettingsDialog {
 
 }
